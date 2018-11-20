6fffa50c85369394c835c316dacfe495fee9fa2c
Daymond Blair <daymond.blair@gmail.com>
2018-11-20 01:44:20 -0800

    initial commit

---
+++ SentimentRequest.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoccerStats
+{
+    public class SentimentRequest
+    {
+        [JsonProperty(PropertyName = "documents")]
+        public List<Document> Documents { get; set; }
+
+    }
+
+    public class Document
+    {
+        public string id { get; set; }
+        public string text { get; set; }
+    }
+}
